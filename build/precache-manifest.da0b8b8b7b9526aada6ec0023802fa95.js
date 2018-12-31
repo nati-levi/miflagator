0ce597f7ba37bec53982e902fc35c18acca89f2a
cc-nati <nati.levi@climacell.co>
2019-01-01 00:32:24 +0200

    feat(): first commit

---
+++ build/precache-manifest.da0b8b8b7b9526aada6ec0023802fa95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64548ecaa4b6ff31eda1",
+    "url": "/static/js/main.64548eca.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "64548ecaa4b6ff31eda1",
+    "url": "/static/css/main.df44960e.chunk.css"
+  },
+  {
+    "revision": "e1ea45b1467b435c440630ee2cf69ff5",
+    "url": "/index.html"
+  }
+];
