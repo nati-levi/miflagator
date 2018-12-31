bdb8db6a7d1ab1b0af29f8c2eb1b65d66e576e37
cc-nati <nati.levi@climacell.co>
2019-01-01 00:36:49 +0200

    Updates

---
+++ precache-manifest.535a8ddff03e2a5e6e9e00dac8e4b7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miflagator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "238e30729d77524a9e1b",
+    "url": "/miflagator/static/js/runtime~main.238e3072.js"
+  },
+  {
+    "revision": "8d9d03155f628e27c7d1",
+    "url": "/miflagator/static/js/main.8d9d0315.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/miflagator/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "8d9d03155f628e27c7d1",
+    "url": "/miflagator/static/css/main.df44960e.chunk.css"
+  },
+  {
+    "revision": "bfb71dfb8961c2705270ea0a4cc205ba",
+    "url": "/miflagator/index.html"
+  }
+];
