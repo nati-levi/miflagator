10958ea895411de5674e5ff23c0d9afe66cc1ee4
cc-nati <nati.levi@climacell.co>
2019-01-01 17:26:10 +0200

    Updates

--- precache-manifest.1de181173936362eabd2305c66a61f31.js
+++ precache-manifest.1de181173936362eabd2305c66a61f31.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/miflagator/static/js/runtime~main.238e3072.js"
   },
   {
-    "revision": "3a60bc66f2991eb93409",
-    "url": "/miflagator/static/js/main.3a60bc66.chunk.js"
+    "revision": "4a03c326f03e631d87e5",
+    "url": "/miflagator/static/js/main.4a03c326.chunk.js"
   },
   {
     "revision": "61b63230e5b0e76c90bd",
     "url": "/miflagator/static/js/1.61b63230.chunk.js"
   },
   {
-    "revision": "3a60bc66f2991eb93409",
-    "url": "/miflagator/static/css/main.d7406d0b.chunk.css"
+    "revision": "4a03c326f03e631d87e5",
+    "url": "/miflagator/static/css/main.261058b4.chunk.css"
   },
   {
-    "revision": "c9cfebbbb534c0b1e0371cc2e9095c2d",
+    "revision": "714068d5db49b92caebdec3d4b4e7056",
     "url": "/miflagator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/miflagator/precache-manifest.1cf48a3fe041eda106356d917a2e7477.js"
+  "/miflagator/precache-manifest.1de181173936362eabd2305c66a61f31.js"
 );
 
 workbox.clientsClaim();
